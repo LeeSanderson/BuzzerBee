508edd0242512225b54b6874bd896a0d14ed2342
LeeSanderson <lee.sanderson@codurance.com>
2025-04-04 09:45:11 +0100

    Add flying bee animation and update header with bee image

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Game from "./Game";
 import "./App.css";
 import { BackgroundMusicProvider } from "./contexts/BackgroundMusicContext";
+import beeImage from "./assets/bee1.png";
 
 const App: React.FC = () => {
   return (
     <BackgroundMusicProvider>
       <div className="App">
         <header className="App-header">
-          <h1>Buzzer Bee Game</h1>
+          <div className="bee-animation">
+            <img src={beeImage} alt="Flying Bee" className="flying-bee" />
+          </div>
         </header>
         <Game />
       </div>
