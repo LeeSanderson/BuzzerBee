7da5802519ef85c1c6b61d494e08f1b26ed2220e
LeeSanderson <lee.sanderson@codurance.com>
2025-04-24 16:54:47 +0100

    Remove flying bee

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import Game from "./Game";
 import "./App.css";
 import { BackgroundMusicProvider } from "./contexts/BackgroundMusicContext";
-import beeImage from "./assets/bee1.png";
 
 const App: React.FC = () => {
   return (
     <BackgroundMusicProvider>
       <div className="App">
-        <header className="App-header">
-          <div className="bee-animation">
-            <img src={beeImage} alt="Flying Bee" className="flying-bee" />
-          </div>
-        </header>
+        <header className="App-header"></header>
         <Game />
       </div>
     </BackgroundMusicProvider>
